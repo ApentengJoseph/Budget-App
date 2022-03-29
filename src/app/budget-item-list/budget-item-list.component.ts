3df9635aba59307a8ff8757fb0cd3fb4e6deaf3c
Apenteng Joseph <apentengjoseph59@gmail.com>
2022-03-29 21:04:25 +0100

    First Budget App commit

---
+++ src/app/budget-item-list/budget-item-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { EditItemModelComponent } from '../edit-item-model/edit-item-model.component';
+
+
+
+@Component({
+  selector: 'app-budget-item-list',
+  templateUrl: './budget-item-list.component.html',
+  styleUrls: ['./budget-item-list.component.scss']
+})
+
+
+
+export class BudgetItemListComponent implements OnInit {
+
+
+
+  @Input() budgetItems!:BudgetItem[];
+  @Output() delete: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<any> = new EventEmitter<any>();
+
+
+  constructor(public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  ondelete(item: BudgetItem) {
+    this.delete.emit(item)
+  }
+
+  onCardClicked(item: BudgetItem) {
+    const dialogRef = this.dialog.open(EditItemModelComponent, {
+      width: '580px',
+      data: item
+    })
+    
+
+    dialogRef.afterClosed().subscribe((result) => {
+      // Check if result has a value
+      if(result) {
+       
+        this.update.emit({
+          old: item,
+          new: result
+        });
+      }
+    })
+  }
+
+}
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
+}
